48fdbf4f160f181d88c5fb39feb9363ba599be1a
adarsh249 <adarshpatel249@gmail.com>
2024-05-05 13:57:29 -0700

    fixing urls

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,12 +2,12 @@
 
 // CONSTANTS
 const RECIPE_URLS = [
-    'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
-    'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
-    'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
-    'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
-    'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
-    'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
 ];
   
   // Run the init() function when the page has loaded
