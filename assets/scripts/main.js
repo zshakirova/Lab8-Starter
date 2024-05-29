a5e5bdb9cc0a0a63b143cf90a2f4754145fa1fb7
zshakirova <zshakirova@ucsd.edu>
2024-05-28 21:40:36 -0700

    Lab 8 changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,70 +2,110 @@
 
 // CONSTANTS
 const RECIPE_URLS = [
-    'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
-    'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
-    'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
-    'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
-    'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
-    'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
-];
-
-// Run the init() function when the page has loaded
-window.addEventListener('DOMContentLoaded', init);
-
-// Starts the program, all function calls trace back here
-async function init() {
-  // initialize ServiceWorker
-  initializeServiceWorker();
-  // Get the recipes from localStorage
-  let recipes;
-  try {
-    recipes = await getRecipes();
-  } catch (err) {
-    console.error(err);
+      'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+      'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+      'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+      'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+      'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+      'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+  ];
+  
+  // Run the init() function when the page has loaded
+  window.addEventListener('DOMContentLoaded', init);
+  
+  // Starts the program, all function calls trace back here
+  async function init() {
+    // initialize ServiceWorker
+    initializeServiceWorker();
+    // Get the recipes from localStorage
+    let recipes;
+    try {
+      recipes = await getRecipes();
+    } catch (err) {
+      console.error(err);
+    }
+    // Add each recipe to the <main> element
+    addRecipesToDocument(recipes);
   }
-  // Add each recipe to the <main> element
-  addRecipesToDocument(recipes);
-}
+  
+  /**
+   * Detects if there's a service worker, then loads it and begins the process
+   * of installing it and getting it running
+   */
+  function initializeServiceWorker() {
+    // EXPLORE - START (All explore numbers start with B)
+    /*******************/
+    // ServiceWorkers have many uses, the most common of which is to manage
+    // local caches, intercept network requests, and conditionally serve from
+    // those local caches. This increases performance since users aren't
+    // re-downloading the same resources every single page visit. This also allows
+    // websites to have some (if not all) functionality offline! I highly
+    // recommend reading up on ServiceWorkers on MDN before continuing.
+    /*******************/
+    // We first must register our ServiceWorker here before any of the code in
+    // sw.js is executed.
+    // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+    if ('serviceWorker' in navigator) {
+      // B2. TODO - Listen for the 'load' event on the window object.
+      window.addEventListener('load', () => {
+        // B3. TODO - Register './sw.js' as a service worker (The MDN article
+        //            "Using Service Workers" will help you here)
+        navigator.serviceWorker.register('./sw.js')
+          .then((registration) => {
+            // B4. TODO - Once the service worker has been successfully registered, console
+            //            log that it was successful.
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          })
+          .catch((error) => {
+            // B5. TODO - In the event that the service worker registration fails, console
+            //            log that it has failed.
+            console.log('ServiceWorker registration failed: ', error);
+          });
+      });
+    } else {
+      console.log('Service workers are not supported.');
+    }
+  }
+  
+  /**
+   * Reads 'recipes' from localStorage and returns an array of
+   * all of the recipes found (parsed, not in string form). If
+   * nothing is found in localStorage, network requests are made to all
+   * of the URLs in RECIPE_URLs, an array is made from those recipes, that
+   * array is saved to localStorage, and then the array is returned.
+   * @returns {Array<Object>} An array of recipes found in localStorage
+   */
+  async function getRecipes() {
+    const recipesFromStorage = localStorage.getItem('recipes');
+    if (recipesFromStorage) {
+      return JSON.parse(recipesFromStorage);
+    }
+    const recipes = [];
+    return new Promise((resolve, reject) => {
 
-/**
- * Detects if there's a service worker, then loads it and begins the process
- * of installing it and getting it running
- */
-function initializeServiceWorker() {
-  // EXPLORE - START (All explore numbers start with B)
-  /*******************/
-  // ServiceWorkers have many uses, the most common of which is to manage
-  // local caches, intercept network requests, and conditionally serve from
-  // those local caches. This increases performance since users aren't
-  // re-downloading the same resources every single page visit. This also allows
-  // websites to have some (if not all) functionality offline! I highly
-  // recommend reading up on ServiceWorkers on MDN before continuing.
-  /*******************/
-  // We first must register our ServiceWorker here before any of the code in
-  // sw.js is executed.
-  // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
-}
+      RECIPE_URLS.forEach(async (url) => {
+        try {
+          const response = await fetch(url);
+          const recipe = await response.json();
+          recipes.push(recipe);
+          if (recipes.length === RECIPE_URLS.length) {
+            saveRecipesToStorage(recipes);
+            resolve(recipes);
+          }
+        } catch (error) {
+          console.error('Failed to fetch recipe:', error);
+          reject(error);
+        }
+      });
+    });
+  }
+  
+  /**
+   * Takes in an array of recipes, converts it to a string, and then
+   * saves that string to 'recipes' in localStorage
+   * @param {Array<Object>} recipes An array of recipes
+   */
 
-/**
- * Reads 'recipes' from localStorage and returns an array of
- * all of the recipes found (parsed, not in string form). If
- * nothing is found in localStorage, network requests are made to all
- * of the URLs in RECIPE_URLs, an array is made from those recipes, that
- * array is saved to localStorage, and then the array is returned.
- * @returns {Array<Object>} An array of recipes found in localStorage
- */
-async function getRecipes() {
-  // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   /**************************/
@@ -100,30 +140,23 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
-}
-
-/**
- * Takes in an array of recipes, converts it to a string, and then
- * saves that string to 'recipes' in localStorage
- * @param {Array<Object>} recipes An array of recipes
- */
-function saveRecipesToStorage(recipes) {
-  localStorage.setItem('recipes', JSON.stringify(recipes));
-}
-
-/**
- * Takes in an array of recipes and for each recipe creates a
- * new <recipe-card> element, adds the recipe data to that card
- * using element.data = {...}, and then appends that new recipe
- * to <main>
- * @param {Array<Object>} recipes An array of recipes
- */
-function addRecipesToDocument(recipes) {
-  if (!recipes) return;
-  let main = document.querySelector('main');
-  recipes.forEach((recipe) => {
-    let recipeCard = document.createElement('recipe-card');
-    recipeCard.data = recipe;
-    main.append(recipeCard);
-  });
-}
+  function saveRecipesToStorage(recipes) {
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+  }
+  
+  /**
+   * Takes in an array of recipes and for each recipe creates a
+   * new <recipe-card> element, adds the recipe data to that card
+   * using element.data = {...}, and then appends that new recipe
+   * to <main>
+   * @param {Array<Object>} recipes An array of recipes
+   */
+  function addRecipesToDocument(recipes) {
+    if (!recipes) return;
+    let main = document.querySelector('main');
+    recipes.forEach((recipe) => {
+      let recipeCard = document.createElement('recipe-card');
+      recipeCard.data = recipe;
+      main.append(recipeCard);
+    });
+  }
--- sw.js
+++ sw.js
@@ -1,40 +1,63 @@
 // sw.js - This file needs to be in the root of the directory to work,
-//         so do not move it next to the other scripts
+//         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-8-starter';
+const RECIPE_URLS = [
+  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+];
 
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-      //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
-    })
-  );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
+      //            added to the cache when the ServiceWorker is installed
+      return cache.addAll(RECIPE_URLS);
+    })
+  );
 });
 
 // Activates the service worker
 self.addEventListener('activate', function (event) {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(self.clients.claim());
 });
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        // B8. TODO - If the request is in the cache, return with the cached version.
+        if (response) {
+          return response;
+        }
+        // Otherwise fetch the resource, add it to the cache, and return network response.
+        return fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+  // We added some known URLs to the cache above, but tracking down every
+  // subsequent network request URL and adding it manually would be very taxing.
+  // We will be adding all of the resources not specified in the intiial cache
+  // list to the cache as they come in.
+  /*******************************/
+  // This article from Google will help with this portion. Before asking ANY
+  // questions about this section, read this article.
+  // NOTE: In the article's code REPLACE fetch(event.request.url) with
+  //       fetch(event.request)
+  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
+  /*******************************/
+  // B7. TODO - Respond to the event by opening the cache using the name we gave
+  //            above (CACHE_NAME)
+  // B8. TODO - If the request is in the cache, return with the cached version.
+  //            Otherwise fetch the resource, add it to the cache, and return
+  //            network response.
 });
